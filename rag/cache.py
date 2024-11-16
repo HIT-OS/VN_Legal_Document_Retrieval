6d5a54c49f42a8ce9bd87b57cc5da4a585e8b11b
phamdt203 <phamdt203@gmail.com>
2024-11-16 11:15:04 +0700

    phamdt203 setup(redis): update save and load cache with redis

---
+++ rag/cache.py
@@ -0,0 +1,14 @@
+import redis
+from dotenv import load_dotenv
+import os
+load_dotenv()
+redis_host = os.getenv("REDIS_HOST") 
+# Create a redis client
+redis_host = 'localhost'
+redisClient = redis.Redis(host=redis_host, port=6379, db=0)
+
+# Check the connection
+if redisClient.ping():
+    print('Connected to Redis server')
+else:
+    print('Failed to connect to Redis server')
